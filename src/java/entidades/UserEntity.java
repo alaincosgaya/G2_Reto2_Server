8a07589493373da60e2756bb2ae5642971f1238b
alaincosgaya <alsucar02@gmail.com>
2021-12-02 12:19:30 +0100

    Cambios en la entidad User, javadoc y tipo de herencia

--- src/java/entidades/UserEntity.java
+++ src/java/entidades/UserEntity.java
@@ -11,12 +11,17 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 
 /**
- * 
+ * Entidad de la clase User.
  * @author Alain Cosgaya
  */
 @Entity
+@Table(name="user",schema="G2Lauserri")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class UserEntity implements Serializable{
     
     private static final Long serialVersionUID = 1L;
@@ -41,77 +46,146 @@ public class UserEntity implements Serializable{
     @Column
     private LocalDateTime lastPasswordChange;
     
-    
-    
+    /**
+     * Constructor vacio de la entidad User.
+     */
    
     public UserEntity() {
         
     }
 
-   
+    /**
+     * Recoger id
+     * @return id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * set id
+     * @param id
+     */
     public void setId(Long id) {
         this.id = id;
     }
-     public String getUsername() {
+
+    /**
+     * recoge username
+     * @return username
+     */
+    public String getUsername() {
         return username;
     }
 
+    /**
+     * set username
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * set email
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Recoge fullName
+     * @return
+     */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * set fullName
+     * @param fullName
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    /**
+     * Recoge password
+     * @return password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * set password
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * recoge userStatus
+     * @return userStatus
+     */
     public UserStatusType getUserStatus() {
         return UserStatus;
     }
 
+    /**
+     * set userStatus
+     * @param UserStatus
+     */
     public void setUserStatus(UserStatusType UserStatus) {
         this.UserStatus = UserStatus;
     }
 
+    /**
+     * recoge userPrivilege
+     * @return
+     */
     public UserPrivilegeType getUserPrivilege() {
         return UserPrivilege;
     }
 
+    /**
+     * set userPrivilege
+     * @param UserPrivilege
+     */
     public void setUserPrivilege(UserPrivilegeType UserPrivilege) {
         this.UserPrivilege = UserPrivilege;
     }
 
+    /**
+     * recoge lastPasswordChange
+     * @return lastPasswordChange
+     */
     public LocalDateTime getLastPasswordChange() {
         return lastPasswordChange;
     }
 
+    /**
+     * set lastPasswordChange
+     * @param lastPasswordChange
+     */
     public void setLastPasswordChange(LocalDateTime lastPasswordChange) {
         this.lastPasswordChange = lastPasswordChange;
     }
 
+    /**
+     * HashCode
+     * @return int
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -126,6 +200,11 @@ public class UserEntity implements Serializable{
         return hash;
     }
 
+    /**
+     * Metodo para comparacion de entidades
+     * @param obj
+     * @return boolean con si es igual o no
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -165,6 +244,10 @@ public class UserEntity implements Serializable{
         return true;
     }
 
+    /**
+     * Metodo toString con datos de la entidad
+     * @return datos de la entidad
+     */
     @Override
     public String toString() {
         return "UserEntity{" + "id=" + id + ", username=" + username + ", email=" + email + ", fullName=" + fullName + ", password=" + password + ", UserStatus=" + UserStatus + ", UserPrivilege=" + UserPrivilege + ", lastPasswordChange=" + lastPasswordChange + '}';
